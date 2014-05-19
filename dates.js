9a6cd029a83a7891ff187dfd0c4682a1988892b5
Arjun Comar <nrujac@gmail.com>
2014-05-19 02:20:26 -0400

    Added date based coloring.

--- dates.js
+++ dates.js
@@ -4,3 +4,6 @@ var filter_dates = function(start_date, end_date, nodes) {
         return date.getTime() <= end_date.getTime() && date.getTime() >= start_date.getTime()
     });
 }
+
+var date_format = d3.time.format("%m/%d/%Y");
+
