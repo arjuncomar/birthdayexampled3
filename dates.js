3eba7d825a7fc859d7006330eca596b78b7fc7e9
Arjun Comar <nrujac@gmail.com>
2014-05-19 01:49:23 -0400

    Added a name to the date compare func.

--- dates.js
+++ dates.js
@@ -1,4 +1,4 @@
-function(start_date, end_date, nodes) {
+var filter_dates = function(start_date, end_date, nodes) {
     return nodes.filter(function(node, i, arr) {
         var date = Date(node.birthday);
         return date.getTime() <= end_date.getTime() && date.getTime() >= start_date.getTime()
